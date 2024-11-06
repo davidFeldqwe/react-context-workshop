80912d53b6f31d1130dd61f65da3c97d5c4e8b4e
davidfeld <davidfeld@hilma.tech>
2024-11-06 14:33:11 +0200

    first commit

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,12 @@
+//make a context called UserContext and a context provider component.
+//the context should control the username
+//the values that are provided to the context should be: username, setUsername
+
+
+
+// 1: 👇create the Context
+
+// 2: 👇create the Provider Component 
+
+
+// (recommended step): create custom hook 🤓
