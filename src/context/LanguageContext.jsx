da81af0a76e922ec9a6dac11c1355858fefd865d
davidfeld <davidfeld@hilma.tech>
2024-11-10 13:58:48 +0200

    docs: added guide comments

--- src/context/LanguageContext.jsx
+++ src/context/LanguageContext.jsx
@@ -1,6 +1,15 @@
-//make a context called LanguageContext and a context provider component.
-//the context should control the language of the website
+//Make a context called LanguageContext and a context provider component.
+//The context should control the language of the website
 
-//the values that are provided to the context should be: language, toggleLanguage
-// create functions in the context provider the toggle the language
-// the language should be Hebrew or English
+// The values that are provided to the context should be: language, toggleLanguage
+// Create functions in the context provider the toggle the language
+// The language should be Hebrew or English
+
+
+
+// 1: 👇create the Context
+
+// 2: 👇create the Provider Component 
+
+
+// (recommended step): create custom hook 🤓
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,6 +1,6 @@
-//make a context called UserContext and a context provider component.
-//the context should control the username
-//the values that are provided to the context should be: username, setUsername
+//Make a context called UserContext and a context provider component.
+//The context should control the username
+//The values that are provided to the context should be: username, setUsername
 
 
 
